16edcb8f523564c928096f3c332b08b10b3b42a9
Neel Patel <neel.patel.2012.np@gmail.com>
2017-03-26 11:57:55 +0530

    configure netbeans project.

---
+++ src/softwareMonitor/SoftwareMonitor.java
@@ -0,0 +1,16 @@
+package softwareMonitor;
+
+/**
+ *
+ * @author Neel Patel
+ */
+public class SoftwareMonitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
