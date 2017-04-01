d2d0ebca754979c5b18bc1bce148b95026377bda
kontactr <gandhiyash1381997@gmail.com>
2017-04-02 00:28:59 +0530

    filter update 2

--- src/client/filter/Filter.java
+++ src/client/filter/Filter.java
@@ -16,11 +16,20 @@
 
 package client.filter;
 
+import com.dataBean.DataBeans;
 import com.dataBean.DataTuple;
+import java.io.DataInput;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.List;
 import java.util.TreeSet;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 /**
  *
  * @author Sony
@@ -33,7 +42,7 @@ public class Filter {
    public Filter(List<String> ls)
     {
         this.ls = ls;
-        //se.addAll(ls);
+       //se.addAll(ls);
     }
     
    
@@ -42,21 +51,31 @@ public class Filter {
        return se.addAll(ls);
    }
    
-    public synchronized Set<String> filterdata()
+    public synchronized List<DataTuple> filterdata()
     {
-        if(ls.size() == 0)
+       /* if(ls.size() == 0)
             return null;
         
         se = new TreeSet<String>();
         if(ls.size() == 0)
             return null;
-        DataTuple dtt = null;
+        DataTuple dtt = null;*/
+     String name="";
+     String ver="";
+     LocalDate date=null;
+    
+        List<String> temp = new ArrayList<String>();
+        
         for(String s:ls)
         {
             
-            if(s.trim().contains("[HKEY_LOCAL_MACHINE"))
+            if(s.trim().contains("[HKEY_LOCAL_MACHINE") || s.equals(ls.get(ls.size()-1)))
             {
-                //dtt = Data
+                   if(name!="" || ver!="" || date!=null)
+                        dt.add((DataTuple) DataBeans.getDataTuple(name, ver, date));
+                    name = "";
+                    ver = "";
+                    date = null;
             }
             
             
@@ -65,12 +84,52 @@ public class Filter {
                 //System.out.println(s);
                 String[] g = s.trim().split("\"DisplayName\"=");
                 //System.out.println(g[1].substring(1, g[1].length()-1));
-                se.add(g[1].substring(1, g[1].length()-1).trim());
+                name = (g[1].substring(1, g[1].length()-1).trim());
+            } else {
+            }
+            
+            if((s.trim().contains("\"DisplayVersion\"=")))
+            {
+                //System.out.println(s);
+                String[] g = s.trim().split("\"DisplayVersion\"=");
+                //System.out.println(g[1].substring(1, g[1].length()-1));
+                ver = (g[1].substring(1, g[1].length()-1).trim());
+            } else {
+            }
+            
+            if((s.trim().contains("\"InstallDate\"=")))
+            {
+                //System.out.println(s);
+                String[] g = s.trim().split("\"InstallDate\"=");
+                String tr = "";//System.out.println(g[1].substring(1, g[1].length()-1));
+                
+                 tr = (g[1].substring(1, g[1].length()-1).trim());
+                 
+                date = LocalDate.of(new Integer(tr.substring(0, 4)).intValue(), new Integer(tr.substring(4, 6)).intValue(), new Integer(tr.substring(6)).intValue());
             } else {
             }
         }
-        return se;
+        //return se;
+         return dt;
     }
+    /*public static void main(String args[]) throws IOException
+    {
+        Path p;
+        p = Paths.get("C:","Users","Sony","Desktop","out1.txt");
+        
+        List<String> ls = null;
+         ls = (Files.readAllLines(p,Charset.forName("UTF-16")));
+         for (DataTuple dt: new Filter(ls).filterdata())
+         {
+             System.out.println(dt.getSoftName());
+             System.out.println(dt.getVersion());
+             System.out.println(dt.getDate());
+         }
+        //for(String s :ls)
+          //  System.out.println(s);
+        
+                
+    }*/
     
 }
 
